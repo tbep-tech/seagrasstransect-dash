b3c8e83b01791d5eec1aa0e896bce04975a7903f
fawda123 <mbafs2012@gmail.com>
2020-12-06 10:13:53 -0500

    dashboard skeleton added

---
+++ R/dat_proc.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(tbeptools)
+
+# transect data -----------------------------------------------------------
+
+# import entire transect dataset as JSON
+transect <- read_transect(training = FALSE)
+
+# get transect species occurrence summaries
+transectocc <- anlz_transectocc(transect)
+
+save(transect, file = 'data/transect.RData', compress = 'xz')
+save(transectocc, file = 'data/transectocc.RData', compress = 'xz')
